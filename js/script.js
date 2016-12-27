ec72d9f5569eb9b1123b9541b62008ef2d7005ca
levg34 <levg34@users.noreply.github.com>
2016-12-27 21:58:41 +0100

    get repositories with github api

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var projectsArea = document.getElementById('projects')
+
+var baseUrl = 'https://api.github.com'
+var requestUrl = baseUrl+'/users/levg34/repos'
+
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest()
+	xmlHttp.open( "GET", theUrl, false ) // false for synchronous request
+	xmlHttp.send( null )
+	return xmlHttp.responseText
+}
+
+var repos = JSON.parse(httpGet(requestUrl))
+
+var res = ''
+
+for (var i in repos) {
+	var repo = repos[i]
+	if (repo.name!='levg34.github.io') {
+		var repoUrl = 'https://levg34.github.io/'+repo.name
+		res += '<a href="'+repoUrl+'">'+repo.name+'</a> - '+repo.description+'<br>'
+	}
+}
+
+projectsArea.innerHTML = res
