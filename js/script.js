388e8be7f6666c4de690a6c7c1b88c97a7881590
levg34 <levg34@users.noreply.github.com>
2017-01-12 14:49:24 +0100

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
 var projectsArea = document.getElementById('projects')
 
 var baseUrl = 'https://api.github.com'
-
-function readTextFile(file) {
-	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", file, false);
-	rawFile.onreadystatechange = function ()
-	{
-		if(rawFile.readyState === 4)
-		{
-			if(rawFile.status === 200 || rawFile.status == 0)
-			{
-				var allText = rawFile.responseText;
-				return(allText);
-			}
-		}
-	}
-	rawFile.send(null);
-}
-
 var user = 'levg34'
 var pass = localStorage.token
 
